6ea17f7fdf394bb291ddac160ee694950c858bdb
dogmom3 <blair.seivers@yahoo.com>
2022-02-11 17:28:24 -0500

    fetch api attempts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-var forecast = document.querySelector('ul');
-var fetchButton = document.getElementById('fetch-button');
+fetch ('http://api.openweathermap.org/geo/1.0/direct?q={Raleigh},{NC},{USA}&limit={5}&appid={key1}')
+.then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
 
-function getApi() {
-var requestUrl = 'http://openweathermap.org';
 
-fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = data[i].html_url;
-            forecast.appendChild(listItem);
-          }
-        console.log(data)
-    });
-}
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=' + 'Raleigh' + '&appid=' + 0ca9bd63c01393dc4b98bf32eef9fed8)
+//     .then(function (response) {
+//         return res.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+
+
+
+
+// api_key = '0ca9bd63c01393dc4b98bf32eef9fed8'
+
+// var forecast = document.querySelector('ul');
+// var fetchButton = document.getElementById('fetch-button');
+
+// function getApi() {
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+// fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//             var listItem = document.createElement('li');
+//             listItem.textContent = data[i].html_url;
+//             forecast.appendChild(listItem);
+//           }
+//         console.log(data)
+//     });
+// }
 
