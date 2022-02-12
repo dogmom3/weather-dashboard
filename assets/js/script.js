f95f48fcecbc24a294f6acb175d15ba781863725
dogmom3 <blair.seivers@yahoo.com>
2022-02-12 13:33:17 -0500

    add event listener/api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,28 @@
-fetch ('http://api.openweathermap.org/geo/1.0/direct?q={Raleigh},{NC},{USA}&limit={5}&appid={key1}')
-.then(function (response) {
+var submitBtnEl = document.getElementById('submit-button');
+var searchInputEl = document.getElementById('search-input');
+
+function findWeatherByCity(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + '0ca9bd63c01393dc4b98bf32eef9fed8')
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log( 'weather: ', data );
+            return { lat: data.coord.lat, lon: data.coord.lon }
         })
+        .then(function (data) {
+            return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.lat + '&lon=' + data.lon + '&appid=0ca9bd63c01393dc4b98bf32eef9fed8')
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('one call', data);
+        })
+    };
+   
 
-
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=' + 'Raleigh' + '&appid=' + 0ca9bd63c01393dc4b98bf32eef9fed8)
-//     .then(function (response) {
-//         return res.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     })
-
-
-
-
-// api_key = '0ca9bd63c01393dc4b98bf32eef9fed8'
-
-// var forecast = document.querySelector('ul');
-// var fetchButton = document.getElementById('fetch-button');
-
-// function getApi() {
-// var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-// fetch(requestUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         for (var i = 0; i < data.length; i++) {
-//             var listItem = document.createElement('li');
-//             listItem.textContent = data[i].html_url;
-//             forecast.appendChild(listItem);
-//           }
-//         console.log(data)
-//     });
-// }
-
+submitBtnEl.addEventListener('click', function(){
+var userInput = searchInputEl.value
+findWeatherByCity(userInput);
+})
